2606ca4bcd61ae9db4cd59d046e72a5dcd940f70
Venkatesh Yandapalli <yandapallivenkatesh94@gmail.com>
2019-04-07 20:03:30 +0530

    Update server.py to include port parsing from env var

--- server.py
+++ server.py
@@ -19,4 +19,9 @@ def generator():
     assembly.main(str(data['msg']))
     return "1"
     
-app.run(debug=True,host= '0.0.0.0')
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True,host='0.0.0.0', port=port)
+
+#app.run(debug=True,host= '0.0.0.0')
